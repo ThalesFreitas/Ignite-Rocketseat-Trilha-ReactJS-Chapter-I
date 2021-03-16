77d3f4dc79e08396b828172079027dc07cc7ea50
Thales Freitas <thalesfreitas10@gmail.com>
2021-03-16 18:41:09 -0300

    Propriedades no React

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,27 +1,21 @@
-const repositoryName = 'unform2';
+import { RepositoryItem } from "./RepositoryItem";
+
+//passando propriedade para o componente filho
+const repository = {
+    name: 'unform',
+    description: 'Forms in React',
+    link: 'https://github.com/unform/unform'
+}
 
 export function RepositoryList() {
     return (
         <section className="repository-list">
             <h1>Lista de repositórios</h1> 
             <ul>
-                <li>
-                    <strong>{repositoryName}</strong>
-                    <p>Forms in React</p>
-
-                    <a href="">
-                        Acessar repositório
-                    </a>
-                </li>
-
-                <li>
-                    <strong>unform</strong>
-                    <p>Forms in React</p>
-
-                    <a href="">
-                        Acessar repositório
-                    </a>
-                </li>
+                <RepositoryItem repository = {repository}/>
+                <RepositoryItem repository = {repository}/>
+                <RepositoryItem repository = {repository}/>
+                <RepositoryItem repository = {repository}/>
             </ul>
         </section>
     );
