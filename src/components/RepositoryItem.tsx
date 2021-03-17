6a98c198ebc56378205d880a9bd6d85dfae310b6
Thales Freitas <thalesfreitas10@gmail.com>
2021-03-17 17:17:10 -0300

    Componentes com TypeScript

--- src/components/RepositoryItem.tsx
+++ src/components/RepositoryItem.tsx
@@ -1,4 +1,12 @@
-export function RepositoryItem(props) {
+interface RepositoryItemProps {
+    repository: {
+        name: string;
+        description: string;
+        html_url: string;
+    }
+}
+
+export function RepositoryItem(props: RepositoryItemProps) {
     return (
         <li>
                     {/* Recebe o dados do repositorio pai */}
--- src/components/RepositoryList.tsx
+++ src/components/RepositoryList.tsx
@@ -3,8 +3,16 @@ import { RepositoryItem } from "./RepositoryItem";
 
 import '../styles/repositories.scss';
 
+interface Repository {
+    name: string;
+    description: string;
+    html_url: string;
+}
+
 export function RepositoryList() {
-        const [repositories, setRepositories] = useState([]);
+    //Tipo generico => useState<> , o estado pode ser do tipo de numero, string, array
+    // o tipo generico define que o estado pode mudar e ser de qualquer tipo.
+        const [repositories, setRepositories] = useState<Repository[]>([]);
 
         useEffect(() => {
             fetch('https://api.github.com/orgs/rocketseat/repos')
